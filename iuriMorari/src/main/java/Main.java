5a9196547805f5a070532959c8f1697915bad99f
Iuri Marari <iuri.marari@endava.com>
2020-04-08 17:25:23 +0300

    [iMarari]Automation School task 2 (OOP)

--- iuriMorari/src/main/java/Main.java
+++ iuriMorari/src/main/java/Main.java
@@ -23,44 +23,54 @@ public class Main {
         User FirstUser = new User("Pedro Malucious", FullAddressOne);
         User SecondUser = new User("Stanislav Groppa", FullAddressTwo);
 
-        //creating item collection1
+        //creating items 1
         List<Item> FirstItemList = new ArrayList<>();
-        Item firstItem = new Item("electronicsItem", Type.PC, 55);
-        Item secondItem = new Item("Boots", Type.BOOTS, 99);
-        Item thirdItem = new Item("Dress", Type.CLOTHES, 66);
-        FirstItemList.add(firstItem);
-        FirstItemList.add(secondItem);
-        FirstItemList.add(thirdItem);
+        Item itemPc = new Item("Electronics", Type.PC, 55.22);
+        Item itemBoots = new Item("Boots", Type.BOOTS, 99.11);
+        Item itemDress = new Item("Dress", Type.CLOTHES, 66.23);
 
+        FirstItemList.add(itemPc);
+        FirstItemList.add(itemBoots);
+        FirstItemList.add(itemDress);
 
-        //creating item collection2
+        //creating items 2
         List<Item> SecondItemList = new ArrayList<>();
-        Item firstItem2 = new Item("T-short", Type.CLOTHES, 44);
-        Item secondItem2 = new Item("Dress2", Type.CLOTHES, 36);
-        FirstItemList.add(firstItem2);
-        FirstItemList.add(secondItem2);
+        Item itemShort = new Item("T-short", Type.CLOTHES, 44.12);
+        Item itemBlazer = new Item("Blazer", Type.CLOTHES, 36.43);
+        Item itemBlazer2 = new Item("Blazer", Type.CLOTHES, 36.43);
+
+        SecondItemList.add(itemShort);
+        SecondItemList.add(itemBlazer);
 
         //creating 2 objects of order
-        Order FirstOrder = new Order(FirstUser, FirstItemList);
-        Order SecondOrder = new Order(SecondUser, SecondItemList);
+        UserOrder FirstOrder = new UserOrder(FirstItemList, FirstUser);
+        UserOrder SecondOrder = new UserOrder(SecondItemList, SecondUser);
+        AdminUser admin = new AdminUser("Mihail Nastase", FullAddressOne);
+        admin.approveOrder(FirstOrder);
+        admin.rejectOrder(FirstOrder);
 
+        FirstOrder.getDate();
+        System.out.println("Today is date: " + FirstOrder.getDate());
         //creating catalog of orders
-        List<Order> OrderCatalog = new ArrayList<>();
+        List<AbstractOrder> OrderCatalog = new ArrayList<>();
         OrderCatalog.add(FirstOrder);
         OrderCatalog.add(SecondOrder);
 
-        //Filter orders by calling function filterOrderByItemType(OrderCatalog) from Order.java
-        List<Order> filteredCatalog = Order.filterOrderByItemType(OrderCatalog, Type.CLOTHES);
+        System.out.println("Total cost of order: " + SecondOrder.sum());
+        System.out.println("Cost of order Blazer: " + SecondOrder.sum(itemBlazer));
+
+        //Filter orders by calling method filterOrderByItemType(OrderCatalog) from UserOrder.java
+        List<AbstractOrder> filteredCatalog = AbstractOrder.filterOrderByItemType(OrderCatalog, Type.CLOTHES);
         System.out.println("The quantity of orders that consist CLOTHES: " + filteredCatalog.size());
 
         //creating catalog of users
-        List<User> CatalogOfUsers = new ArrayList<>();
+        List<AbstractUser> CatalogOfUsers = new ArrayList<>();
         CatalogOfUsers.add(FirstUser);
         CatalogOfUsers.add(SecondUser);
 
         //Filter orders by calling method filterUsersByCity(OrderCatalog) from user.java
-        List<User> UserCatalog;
-        UserCatalog = User.filterUsersByCity(CatalogOfUsers, "Chisinau"); // call for viewing the users from Chisinau city
+        List<AbstractUser> UserCatalog;
+        UserCatalog = AbstractUser.filterUsersByCity(CatalogOfUsers, "Chisinau"); // call for viewing the users from Chisinau city
         System.out.println("The filtered list of users from city Chisinau is: " + UserCatalog);
 
 
--- iuriMorari/src/main/java/Order.java
+++ iuriMorari/src/main/java/Order.java
@@ -1,41 +1,9 @@
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Order {
-    public long id;
-    public LocalDateTime date;
-    public User user;
-    public List<Item> items;
-    static long idCounter;
+public interface Order {
+    public double sum();
 
-    public Order(User user, List<Item> items) {
-        this.id = ++idCounter;
-        this.date = LocalDateTime.now();
-        this.user = user;
-        this.items = items;
-    }
+    public int count();
 
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", date=" + date +
-                ", user=" + user +
-                ", items=" + items +
-                '}';
-    }
-
-    // creating function to filter list of orders that have clothes
-    public static List<Order> filterOrderByItemType(List<Order> ListOfOrders, Type type) {
-        List<Order> orderResult = new ArrayList<>();
-        for (Order order : ListOfOrders) {
-            for (Item item : order.items) {
-                if (type == item.type) {
-                    orderResult.add(order);
-                }
-            }
-        }
-        return orderResult;
-    }
+    public LocalDateTime dateTime();
 }
